8a8aef5b1f690d317d11c6ffb824ce41329057b2
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-04-20 15:46:52 +0200

    first commit

---
+++ lib/Features/splach/presentation/view/splach_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/Features/splach/presentation/view/widget/splach_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplachView extends StatelessWidget {
+  const SplachView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplachViewBody();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/Features/splach/presentation/view_models/view/splach_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplachView(),
+    );
+  }
+}
