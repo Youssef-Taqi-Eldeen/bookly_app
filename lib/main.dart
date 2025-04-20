18fc619ad1d0d2a7f4d0a700d3f6b9d67e95758e
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-04-20 19:35:31 +0200

    create project and create splach view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/Features/splach/presentation/view_models/view/splach_view.dart';
+import 'package:bookly/Features/splach/presentation/view/splach_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
