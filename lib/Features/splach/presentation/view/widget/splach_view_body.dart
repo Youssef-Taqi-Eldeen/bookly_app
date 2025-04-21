163c8cbe4c8e29f1a8715bbbda20dfffcd27beb5
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-04-22 00:44:13 +0200

    Adding sliding animation to splach view

--- lib/Features/splach/presentation/view/widget/splach_view_body.dart
+++ lib/Features/splach/presentation/view/widget/splach_view_body.dart
@@ -1,8 +1,42 @@
+import 'package:bookly/Features/splach/presentation/view/widget/sliding_text.dart';
 import 'package:bookly/core/utils/asstes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,10 +48,7 @@ class SplachViewBody extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
