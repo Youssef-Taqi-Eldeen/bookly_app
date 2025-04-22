1ce0b88f3967aa087f43f60817ed24dbf278665d
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-04-23 00:49:35 +0200

    add custom app bar to home

---
+++ lib/Features/home/presentation/view/widget/home_view_body.dart
@@ -0,0 +1,39 @@
+import 'package:bookly/core/utils/asstes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [CustomAppBar()],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        children: [
+          Image.asset(
+            AsstesData.logo,
+            height: 18,
+          ),
+          const Spacer(),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(FontAwesomeIcons.magnifyingGlass),
+          ),
+        ],
+      ),
+    );
+  }
+}
