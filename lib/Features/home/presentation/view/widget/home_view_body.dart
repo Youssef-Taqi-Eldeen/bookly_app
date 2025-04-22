1b8ca28e84ae471354af447514fd4fbfd7de4465
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-04-23 00:51:35 +0200

    refactor home view body

--- lib/Features/home/presentation/view/widget/home_view_body.dart
+++ lib/Features/home/presentation/view/widget/home_view_body.dart
@@ -1,7 +1,6 @@
-import 'package:bookly/core/utils/asstes.dart';
+import 'package:bookly/Features/home/presentation/view/widget/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,30 +9,6 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [CustomAppBar()],
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
-      child: Row(
-        children: [
-          Image.asset(
-            AsstesData.logo,
-            height: 18,
-          ),
-          const Spacer(),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(FontAwesomeIcons.magnifyingGlass),
-          ),
-        ],
-      ),
     );
   }
 }
